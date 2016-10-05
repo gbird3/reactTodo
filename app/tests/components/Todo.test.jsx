db21e29f10b81ab0b3d973aecaa9d8b9f72a39ab
Gregory Bird <Gregory Bird>
2016-10-05 10:19:10 -0600

    add toggle support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -6,8 +6,23 @@ var TestUtils = require('react-addons-test-utils');
 
 var Todo = require('Todo');
 
-describe('Todo app', () => {
+describe('Todo', () => {
   it('should exist', () => {
     expect(Todo).toExist();
+  });
+
+  it('should call onToggle prop with id on click', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: false
+    };
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith(11);
   });
 });
