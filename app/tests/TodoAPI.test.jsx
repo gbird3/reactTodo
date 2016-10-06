03503de0076c3fd37dc89f109c771349c800649d
Gregory Bird <Gregory Bird>
2016-10-06 09:03:43 -0600

    add filter funcationality

--- app/tests/TodoAPI.test.jsx
+++ app/tests/TodoAPI.test.jsx
@@ -55,6 +55,46 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
 
     });
+  });
 
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'some text',
+      completed: true
+    },{
+      id: 2,
+      text: 'some text here',
+      completed: false
+    },,{
+      id: 3,
+      text: '3',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only uncompleted when showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by search text', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if search text is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
   });
 })
