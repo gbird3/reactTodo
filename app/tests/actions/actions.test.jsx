9d9be3e077aae11fee47eb7449ca3558e6a84f4c
Gregory Bird <gregbird12@gmail.com>
2016-12-29 11:51:21 -0700

    Fetch todos from firebase on app start

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -96,13 +96,18 @@ describe('Actions', () => {
     var testTodoRef;
 
     beforeEach((done) => {
-      testTodoRef = firebaseRef.child('todos').push();
+      var todosRef = firebaseRef.child('todos');
 
-      testTodoRef.set({
+      todosRef.remove().then(() => {
+        testTodoRef = firebaseRef.child('todos').push();
+        
+        return testTodoRef.set({
         text: 'Someting to do',
         compeleted: false,
         createdAt: 12341323,
+      })
       }).then(() => done())
+      .catch(done);
     });
 
 
@@ -127,6 +132,22 @@ describe('Actions', () => {
         expect(mockActions[0].updates.completedAt).toExist();
         done();
       }, done);
+    });
+
+    it('should populate todos and dispatch ADD_TODOS', (done) => {
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+      
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0].type).toEqual('ADD_TODOS');
+        expect(mockActions[0].todos.length).toEqual(1);
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
+
+        done();
+
+      }, done)
     });
   });
 });
