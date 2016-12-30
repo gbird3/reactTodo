ea7a71e9160a1b2d53dcc63b01ffe23d8381605a
Gregory Bird <gregbird12@gmail.com>
2016-12-30 12:26:31 -0700

    Add react router middleware and redirect on auth

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -93,9 +93,9 @@ export var startToggleTodo = (id, completed) => {
 export var startLogin = () => {
   return (dispatch, getState) => {
     return firebase.auth().signInWithPopup(githubProvider).then((result) => {
-      console.log('Auth worked', result);
+      console.log('Auth worked!', result);
     }, (error) => {
-      console.log('Unable to Auth', error);
+      console.log('Unable to auth', error);
     });
   };
 };
@@ -103,7 +103,7 @@ export var startLogin = () => {
 export var startLogout = () => {
   return (dispatch, getState) => {
     return firebase.auth().signOut().then(() => {
-      console.log('Logged out');
-    })
+      console.log('Logged out!');
+    });
   };
 };
